c3ee8e097810862a47a0a1864b940923f5d82e64
yurishe0 <yurishe00@gmail.com>
2025-06-04 21:53:38 +0200

    feat(root): display remaining seconds and refetch exchange rate upon expiring

--- frontend/src/app/(root)/page.tsx
+++ frontend/src/app/(root)/page.tsx
@@ -1,13 +1,18 @@
 import styles from "./root.module.css";
 import ExchangeForm from "@/components/ExchangeForm/ExchangeForm";
+import ExchangeTimer from "@/components/ExchangeTimer/ExchangeTimer";
 import getExchangeRate from "@/utils/getExchangeRate";
 
 export default async function Home() {
 	const exchangeRate = await getExchangeRate();
+
+	console.log(exchangeRate);
 	return (
 		<div className={styles.homepage}>
 			<h1>Exchange EUR to PLN</h1>
-			<p>Current exchange rate: {exchangeRate}</p>
+			<p>Current exchange rate: {exchangeRate.data.rate}</p>
+			<p>{exchangeRate.data.secondsLeft}</p>
+			<ExchangeTimer initialSeconds={exchangeRate.data.secondsLeft} />
 			<ExchangeForm />
 		</div>
 	);
---
+++ frontend/src/components/ExchangeTimer/ExchangeTimer.tsx
@@ -0,0 +1,23 @@
+'use client';
+
+import { useEffect, useState } from 'react';
+import { useRouter } from 'next/navigation';
+
+export default function ExchangeTimer({ initialSeconds = 60 }) {
+    const [seconds, setSeconds] = useState(initialSeconds);
+    const router = useRouter();
+
+    useEffect(() => {
+        if (seconds === 0) {
+            // Refresh SSR data (initial exchange rate)
+            router.refresh();
+            setSeconds(initialSeconds);
+        }
+
+        const timer = setTimeout(() => setSeconds(seconds - 1), 1000);
+
+        return () => clearTimeout(timer);
+    }, [seconds, initialSeconds]);
+
+    return <p>Refreshing in {seconds}s</p>;
+}
